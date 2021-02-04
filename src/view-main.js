610ac31ca437df36b41290c83d7a977c429c8eb2
isaiahaiasi <isaiah.althouse@gmail.com>
2021-02-04 00:19:57 -0700

    Add basic style

---
+++ src/view-main.js
@@ -0,0 +1,9 @@
+// VIEW module for the shared elements (ie, the header, the nav, & the footer)
+
+// Load "hero" content
+
+// Load nav content
+
+// Create a space for the Main content to go
+
+// Load footer content
